88c93823153b31b538ec8461b1713af037510438
Elianehbmna <elianehabimana3@gmail.com>
2019-09-06 15:01:03 +0200

    adding save and create into run

--- run.py
+++ run.py
@@ -2,18 +2,18 @@
 
 from passwordLock import User, Credential
 
-# def create_user(fname,lname,password):
-# 	'''
-# 	Function to create a new user account
-# 	'''
-# 	new_user = User(fname,lname,password)
-# 	return new_user
+def create_user(fname,lname,password):
+	'''
+	Function to create a new user account
+	'''
+	new_user = User(fname,lname,password)
+	return new_user
 
-# def save_user(user):
-# 	'''
-# 	Function to save a new user account
-# 	'''
-# 	User.save_user(user)
+def save_user(user):
+	'''
+	Function to save a new user account
+	'''
+	User.save_user(user)
 
 
 # def verify_user(first_name,password):
