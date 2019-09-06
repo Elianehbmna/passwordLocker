b9e9b7ecc854c55920c50f5f890fbf4788e1ba6e
Elianehbmna <elianehabimana3@gmail.com>
2019-09-06 15:31:49 +0200

    creating credential cc

--- run.py
+++ run.py
@@ -98,32 +98,32 @@ def main():
 						print(" ")
 						print(f'Goodbye {user_name}')
 						break
-# 					elif short_code == 'cc':
-# 						print(' ')
-# 						print('Enter your credential details:')
-# 						site_name = input('Enter the site\'s name- ').strip()
-# 						user_name = input('Enter your account\'s name - ').strip()
-# 						while True:
-# 							print(' ')
-# 							print("-"*60)
-# 							print('Please choose an option for entering a password: \n ep-enter existing password \n gp-generate a password \n ex-exit')
-# 							psw_choice = input('Enter an option: ').lower().strip()
-# 							print("-"*60)
-# 							if psw_choice == 'ep':
-# 								print(" ")
-# 								password = input('Enter your password: ').strip()
-# 								break
-# 							elif psw_choice == 'gp':
-# 								password = generate_password()
-# 								break
-# 							elif psw_choice == 'ex':
-# 								break
-# 							else:
-# 								print('Oops! Wrong option entered. Try again.')
-# 						save_credential(create_credential(user_name,site_name,password))
-# 						print(' ')
-# 						print(f'Credential Created: Site Name: {site_name} - Account Name: {user_name} - Password: {password}')
-# 						print(' ')
+					elif short_code == 'cc':
+						print(' ')
+						print('Enter your credential details:')
+						site_name = input('Enter the site\'s name- ').strip()
+						user_name = input('Enter your account\'s name - ').strip()
+						while True:
+							print(' ')
+							print("-"*60)
+							print('Please choose an option for entering a password: \n ep-enter existing password \n gp-generate a password \n ex-exit')
+							psw_choice = input('Enter an option: ').lower().strip()
+							print("-"*60)
+							if psw_choice == 'ep':
+								print(" ")
+								password = input('Enter your password: ').strip()
+								break
+							elif psw_choice == 'gp':
+								password = generate_password()
+								break
+							elif psw_choice == 'ex':
+								break
+							else:
+								print('Oops! Wrong option entered. Try again.')
+						save_credential(create_credential(user_name,site_name,password))
+						print(' ')
+						print(f'Credential Created: Site Name: {site_name} - Account Name: {user_name} - Password: {password}')
+						print(' ')
 # 					elif short_code == 'dc':
 # 						print(' ')
 # 						if display_credentials(user_name):
